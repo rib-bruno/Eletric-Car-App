e81eb70efacacc51cf71ccdaaf93b8758b2f7459
rib-bruno <ribeirobrunosouza@hotmail.com>
2023-10-06 16:18:49 -0300

    manipulacao e recuperacao de valor na main

--- app/src/main/java/com/example/eletriccarapp/MainActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/MainActivity.kt
@@ -3,28 +3,39 @@ package com.example.eletriccarapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var preco: EditText
-    lateinit var calcular: Button
+    lateinit var kmPercorrido: EditText
+    lateinit var btnCalcular: Button
+    lateinit var resultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setUpView()
+        setupView()
         setupListeners()
     }
 
-    fun setUpView() {
-        calcular = findViewById(R.id.btn_calcular)
+    fun setupView() {
         preco = findViewById(R.id.et_preco_kwh)
+        kmPercorrido = findViewById(R.id.et_km_percorrido)
+        resultado = findViewById(R.id.tv_resultado)
+        btnCalcular = findViewById(R.id.btn_calcular)
     }
 
     fun setupListeners() {
-        calcular.setOnClickListener {
-            val textoDigitado = preco.text.toString()
-            Log.d("texto digitado", textoDigitado)
+        btnCalcular.setOnClickListener {
+            calcular()
         }
+    }
+
+    fun calcular() {
+        val preco = preco.text.toString().toFloat()
+        val km = kmPercorrido.text.toString().toFloat()
+        val result = preco / km
+
+        resultado.text = result.toString()
     }
 }
