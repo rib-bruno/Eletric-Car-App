2b8622fbe350ea963de06b56a7f5e289dfcf1572
rib-bruno <ribeirobrunosouza@hotmail.com>
2023-10-06 12:02:34 -0300

    recuperando valor edittext

--- app/src/main/java/com/example/eletriccarapp/MainActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.eletriccarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var preco: EditText
+    lateinit var calcular: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpView()
+        setupListeners()
+    }
+
+    fun setUpView() {
+        calcular = findViewById(R.id.btn_calcular)
+        preco = findViewById(R.id.et_preco_kwh)
+    }
+
+    fun setupListeners() {
+        calcular.setOnClickListener {
+            val textoDigitado = preco.text.toString()
+            Log.d("texto digitado", textoDigitado)
+        }
     }
 }
