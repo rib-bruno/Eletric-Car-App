21ee6d7d17747431a2f85ed4fe1fb86774f68b2d
rib-bruno <ribeirobrunosouza@hotmail.com>
2023-10-10 18:14:09 -0300

    criacao do recyclerview e adapter

--- app/src/main/java/com/example/eletriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/presentation/MainActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import androidx.recyclerview.widget.RecyclerView
 import com.example.eletriccarapp.R
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnCalcular: Button
+    lateinit var listaCarros: RecyclerView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,16 +18,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setupView()
         setupListeners()
+        setupList()
     }
 
     fun setupView() {
         btnCalcular = findViewById(R.id.btn_calcular)
+        listaCarros = findViewById(R.id.rv_lista_carros)
     }
+
+    fun setupList() {
+        var dados = arrayOf(
+            "Cupcacke",
+            "Donut",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean"
+        )
+       // val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dados)
+      //  lista.adapter = adapter
+    }
+
+
     fun setupListeners() {
         btnCalcular.setOnClickListener {
             // calcular()
             startActivity(Intent(this, CalcularAutonomiaActivity::class.java))
         }
+
     }
 
 }
---
+++ app/src/main/java/com/example/eletriccarapp/presentation/adapter/CarAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.eletriccarapp.presentation.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CarAdapter : RecyclerView.Adapter<> {
+}
+
+class ViewHolder(view:View) : RecyclerView.ViewHolder() {
+
+}
