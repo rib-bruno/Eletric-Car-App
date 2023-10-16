effd73c9e0aada1f034036315fdab7eeba574a7a
rib-bruno <ribeirobrunosouza@hotmail.com>
2023-10-16 10:18:21 -0300

    criação e implementação dos tabs layouts e fragments

--- app/src/main/java/com/example/eletriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/presentation/MainActivity.kt
@@ -1,32 +1,44 @@
 package com.example.eletriccarapp.presentation
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.*
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.example.eletriccarapp.R
 import com.example.eletriccarapp.data.CarFactory
 import com.example.eletriccarapp.presentation.adapter.CarAdapter
+import com.example.eletriccarapp.presentation.adapter.TabsAdapter
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnCalcular: Button
     lateinit var listaCarros: RecyclerView
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager: ViewPager2
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("Lifecycle", "CREATE")
+
         setContentView(R.layout.activity_main)
         setupView()
         setupListeners()
         setupList()
+        setupTabs()
     }
 
+
     fun setupView() {
+        tabLayout = findViewById(R.id.tab_layout)
         btnCalcular = findViewById(R.id.btn_calcular)
         listaCarros = findViewById(R.id.rv_lista_carros)
+        viewPager = findViewById(R.id.vp_viewpager)
     }
 
     fun setupList() {
@@ -36,6 +48,32 @@ class MainActivity : AppCompatActivity() {
         listaCarros.adapter = adapter
     }
 
+    fun setupTabs() {
+        val tabsAdapter = TabsAdapter(this)
+        viewPager.adapter = tabsAdapter
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.let {
+                    viewPager.currentItem = it.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+        //garante que quando to sobrescrevendo um listener da tab, quando selecionar a que eu preciso, vou fazer com que essa fique selecionada
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.getTabAt(position)?.select()
+            }
+        })
+    }
 
     fun setupListeners() {
         btnCalcular.setOnClickListener {
